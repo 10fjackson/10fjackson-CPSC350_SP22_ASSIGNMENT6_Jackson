6812a1804f6df2ceb5d06ce563e156cc90eb7bf3
root <tobijthowe@gmail.com>
2022-05-10 01:27:15 +0000

    Added stack for rollback

--- DataBase.h
+++ DataBase.h
@@ -5,6 +5,7 @@
 #include "Person.h"
 #include "Student.h"
 #include "Faculty.h"
+#include "GenStack.h"
 using namespace std;
 class DataBase
 {
@@ -18,29 +19,41 @@ class DataBase
     private:
         BST<Student> *masterStudent;
         BST<Faculty> *masterFaculty;
+        GenStack<Student> *studentRollback;
+        GenStack<Faculty> *facultyRollback;
 };
 
 DataBase::DataBase(){
     masterStudent = new BST<Student>();
     masterFaculty = new BST<Faculty>();
+    studentRollback = new GenStack<Student>();
+    facultyRollback = new GenStack<Faculty>();
 }
 DataBase::~DataBase(){}
 
 
 DataBase::addStudent(Student *s){
     masterStudent->insert(s);
+    studentRollback->push(s);
 }
 
 DataBase::addFaculty(Faculty *f){
     masterFaculty->insert(f);
+    studentRollback->push(f);
 }
 
 DataBase::removeStudent(Student *s){
     masterStudent->deleteNode(s);
+    studentRollback->push(s);
 }
 
 DataBase::removeFaculty(Faculty *f){
     masterFaculty->deleteNode(f);
+    facultyRollback->push(f);
+}
+
+DataBase::rollback(){
+    
 }
 
 
